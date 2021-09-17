0f1ef107432e526a37797ebd719bc2594c481117
Jefflol <jeffreychen246@gmail.com>
2021-09-16 21:16:00 -0700

    Add request for fetching APOD data with date range

--- src/requests.js
+++ src/requests.js
@@ -12,3 +12,27 @@ export const getApodData = async () => {
         console.error(err)
     }
 }
+
+export const getApodDataWithRange = async (startDate, endDate) => {
+    try {
+        const key = import.meta.env.VITE_NASA_API_KEY
+        const url = `${BASE_URL}/planetary/apod?api_key=${key}&start_date=${startDate}&end_date=${endDate}`
+
+        const res = await fetch(url)
+        const data = await res.json()
+        return data
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+export const debounce = (fn, delay) => {
+    let timer
+    return function() {
+        let context = this
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.apply(context, arguments)
+        }, delay)
+    }
+}
